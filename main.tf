cfc6e93e2af5046a1cf6459fbeea913ac90a745d
Chris Wright <chris@dxw.com>
2020-09-23 14:50:27 +0100

    (fix) terraform 0.12 depriciation warnings

--- main.tf
+++ main.tf
@@ -14,7 +14,7 @@ locals {
 ### IAM
 
 data "template_file" "cloudtrail_s3_policy_template" {
-  template = "${file("${path.module}/policies/cloudtrail_s3_policy.tpl")}"
+  template = file("${path.module}/policies/cloudtrail_s3_policy.tpl")
 
   vars = {
     bucket_name = "${local.bucketname}"
@@ -25,57 +25,57 @@ data "template_file" "cloudtrail_s3_policy_template" {
 
 resource "aws_cloudwatch_log_group" "cloudtrail" {
   name              = "${var.namespace}-${var.cloudwatch_log_group_name}"
-  retention_in_days = "${var.cloudtrail_log_retention}"
+  retention_in_days = var.cloudtrail_log_retention
 
-  tags = "${merge(
+  tags = merge(
     local.default_tags,
     var.additional_tags
-  )}"
+  )
 }
 
 resource "aws_s3_bucket" "cloudtrail" {
-  bucket = "${local.bucketname}"
+  bucket = local.bucketname
   acl    = "private"
-  policy = "${data.template_file.cloudtrail_s3_policy_template.rendered}"
+  policy = data.template_file.cloudtrail_s3_policy_template.rendered
 
-  tags = "${merge(
+  tags = merge(
     local.default_tags,
     var.additional_tags,
-    map("Name", "${local.bucketname}")
-  )}"
+    map("Name", local.bucketname)
+  )
 }
 
 resource "aws_iam_role" "cloudtrail_cloudwatch_logs_role" {
   name               = "${var.namespace}-cloudtrail-cloudwatch-logs"
   path               = "/"
-  assume_role_policy = "${file("${path.module}/policies/cloudtrail_assume_policy.json")}"
+  assume_role_policy = file("${path.module}/policies/cloudtrail_assume_policy.json")
 }
 
 data "template_file" "cloudtrail_cloudwatch_logs_policy_template" {
-  template = "${file("${path.module}/policies/cloudtrail_cloudwatch_logs_policy.tpl")}"
+  template = file("${path.module}/policies/cloudtrail_cloudwatch_logs_policy.tpl")
 
   vars = {
-    cloudwatch_log_group_arn = "${aws_cloudwatch_log_group.cloudtrail.arn}"
+    cloudwatch_log_group_arn = aws_cloudwatch_log_group.cloudtrail.arn
   }
 }
 
 resource "aws_iam_policy" "cloudtrail_cloudwatch_logs_policy" {
   name   = "${var.namespace}-cloudtrail-cloudwatch-logs"
   path   = "/"
-  policy = "${data.template_file.cloudtrail_cloudwatch_logs_policy_template.rendered}"
+  policy = data.template_file.cloudtrail_cloudwatch_logs_policy_template.rendered
 }
 
 resource "aws_iam_role_policy_attachment" "cloudtrail_cloudwatch_logs_policy_attachment" {
-  role       = "${aws_iam_role.cloudtrail_cloudwatch_logs_role.name}"
-  policy_arn = "${aws_iam_policy.cloudtrail_cloudwatch_logs_policy.arn}"
+  role       = aws_iam_role.cloudtrail_cloudwatch_logs_role.name
+  policy_arn = aws_iam_policy.cloudtrail_cloudwatch_logs_policy.arn
 }
 
 resource "aws_cloudtrail" "cloudtrail" {
   name                          = "${var.namespace}-${var.cloudtrail_name}"
-  s3_bucket_name                = "${aws_s3_bucket.cloudtrail.id}"
+  s3_bucket_name                = aws_s3_bucket.cloudtrail.id
   include_global_service_events = true
   is_multi_region_trail         = true
-  cloud_watch_logs_role_arn     = "${aws_iam_role.cloudtrail_cloudwatch_logs_role.arn}"
-  cloud_watch_logs_group_arn    = "${aws_cloudwatch_log_group.cloudtrail.arn}"
+  cloud_watch_logs_role_arn     = aws_iam_role.cloudtrail_cloudwatch_logs_role.arn
+  cloud_watch_logs_group_arn    = aws_cloudwatch_log_group.cloudtrail.arn
   enable_log_file_validation    = true
 }
--- variables.tf
+++ variables.tf
@@ -1,35 +1,30 @@
 variable "additional_tags" {
   default     = {}
   description = "The tags to apply to resources created by this module"
-  type        = "map"
+  type        = map
 }
 
 variable "bucketname" {
   default     = "cloudtrail"
   description = "S3 bucketname to store cloudtrail events in"
-  type        = "string"
 }
 
 variable "cloudtrail_log_retention" {
   default     = "90"
   description = "Number of days to retain cloudtrail logs"
-  type        = "string"
 }
 
 variable "cloudtrail_name" {
   default     = "cloudtrail-all"
   description = "CloudTrail name. Combined with namespace to make it unique"
-  type        = "string"
 }
 
 variable "cloudwatch_log_group_name" {
   default     = "cloudtrail/logs"
   description = "Cloudwatch log group name to send Cloudtrail events to"
-  type        = "string"
 }
 
 variable "namespace" {
   default     = "cloudtrail"
   description = "An identifier used to group all resources created by this module"
-  type        = "string"
 }
